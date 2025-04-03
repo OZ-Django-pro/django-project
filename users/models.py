5a71b9e10e1cffbb84a451846d03af22558297fe
hyunyoung97 <gusdud04164@gmail.com>
2025-04-03 10:58:46 +0900

    feat: users/models.py 수정

--- users/models.py
+++ users/models.py
@@ -11,13 +11,13 @@ class User(AbstractUser):
     is_admin = models.BooleanField(default=False)  # 관리자 여부 (기본값: False)
 
     # 옵션이 추가된 기본 필드
-    email = models.EmailField(unique=True, max_length=50)
+    email = models.EmailField(unique=True, max_length=50) # 이메일 중복 가입 방지 (unique=True 사용)
     is_active = models.BooleanField(default=True) # 계정 활성화 여부 (기본값: True)
 
     # USERNAME_FIELD: 로그인 시 사용할 필드 (기본값은 'username', 여기서는 'email'로 변경)
     USERNAME_FIELD = 'email'
     # REQUIRED_FIELDS: 사용자 생성 시 필수로 입력해야 하는 필드
-    REQUIRED_FIELDS = ['nickname', 'name', 'phone_number']
+    REQUIRED_FIELDS = ['name', 'phone_number']
 
     def __str__(self):
         return self.email  # 객체를 문자열로 표현할 때 이메일 반환
